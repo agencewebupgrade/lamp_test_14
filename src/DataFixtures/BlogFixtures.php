99eaf72c5389c4b877ab96d6dc3a613871a94118
Nicolas MEUNIER <contact@agence-upgrade.fr>
2021-08-10 19:24:46 +0200

    Ajout des fixtures

---
+++ src/DataFixtures/BlogFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+// ADED BY ME
+use App\Entiry\Blog;
+
+class BlogFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        for ($count = 0; $count < 20; $count++) {
+            $blog = new Blog();
+            $blog->setName("Article " . $count);
+            $blog->setImage("image" . $count . ".png");
+            $manager->persist($blog);
+        }
+
+        $manager->flush();
+    }
+}
